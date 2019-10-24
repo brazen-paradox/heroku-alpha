fbba8b1f48e31cbc71274364a26d8aad3a55cfcc
brazen-paradox <brazenparadox@gmail.com>
2019-10-24 08:48:50 +0530

    port left to default

--- flask_hello_world.py
+++ flask_hello_world.py
@@ -14,5 +14,6 @@ def apollo():
     return "tranquility base here.. The Eagle has landed"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port = 6060)
+
+        app.run(host="0.0.0.0")
 
